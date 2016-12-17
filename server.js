dc3667230cde0f9614a5b6dfe9178e5994e9b4e2
EddyTheDove <EddyTheDove@users.noreply.github.com>
2016-12-18 02:46:47 +1100

    removed node-sass-middleware. Using gulp-sass

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
 
 
-app.use(sassMiddleware({
-    src: path.join(__dirname, 'assets', 'sass'),
-    dest: path.join(__dirname, 'public', 'assets', 'css'),
-    debug: true,
-    outputStyle: 'compressed'
-}))
 app.use('/', express.static(__dirname + '/public'))
 app.use(webRouter)
 app.use(apiRouter)
