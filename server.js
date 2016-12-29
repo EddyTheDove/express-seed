0c6402ed560375bd797a618043ddaa2b3e62a4f2
EddyTheDove <eddy.insearch@gmail.com>
2016-12-30 02:54:06 +1100

    app use boduparser

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 
-import express from 'express';
-import path from 'path';
-import webRoutes from './routes/web';
-import apiRoutes from './routes/api';
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import webRoutes from './routes/web'
+import apiRoutes from './routes/api'
 
 let app = express();
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
+
+//bodyParser will let us get data from POST
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 
 app.use('/', express.static(__dirname + '/public'))
